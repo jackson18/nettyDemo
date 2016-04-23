9c62db50d76ec0fb617593bab22d53955de8e198
jackson18 <qijiabin18@126.com>
2016-04-23 11:43:36 +0800

    半包处理

--- src/main/java/com/qijiabin/netty/start/TimeClientHandler.java
+++ src/main/java/com/qijiabin/netty/start/TimeClientHandler.java
@@ -17,32 +17,47 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class TimeClientHandler extends ChannelHandlerAdapter {
 
-	private final ByteBuf message;
+	private byte[] req;
+	private int count;
 	
 	public TimeClientHandler() {
-		byte[] req = "hello world".getBytes();
-		message = Unpooled.buffer(req.length);
-		message.writeBytes(req);
+		//请求数据加上换行
+		String msg = "hello world" + System.getProperty("line.separator");
+		req = msg.getBytes();
 	}
 	
+	/**
+	 * 当客户端主动链接服务端的链接后，这个通道就是活跃的了。
+	 * 也就是客户端与服务端建立了通信通道并且可以传输数据
+	 */
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) {
-		ctx.writeAndFlush(message);
+		ByteBuf buf = null;
+		for (int i = 0; i < 100; i++) {
+			buf = Unpooled.buffer(req.length);
+			buf.writeBytes(req);
+			ctx.writeAndFlush(buf);
+		}
 	}
 	
+	/**
+	 * 简而言之就是从通道中读取数据，也就是服务端接收客户端发来的数据
+	 * 但是这个数据在不进行解码时它是ByteBuf类型
+	 */
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf buf = (ByteBuf) msg;
-		byte[] req = new byte[buf.readableBytes()];
-		buf.readBytes(req);
-		String body = new String(req, "utf-8");
-		System.out.println("result: " + body);
+		String body = (String) msg;
+		System.out.println("result: " + body + ",count: " + ++count);
 	}
 	
+	/**
+	 * 抓住异常，当发生异常的时候，可以做一些相应的处理，比如打印日志、关闭链接
+	 */
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		//释放资源
 		ctx.close();
+		System.out.println("异常信息：\r\n"+cause.getMessage());
 	}
 	
 }
--- src/main/java/com/qijiabin/netty/start/TimeServerHandler.java
+++ src/main/java/com/qijiabin/netty/start/TimeServerHandler.java
@@ -18,27 +18,56 @@ import io.netty.channel.ChannelHandlerContext;
  * 修订日期     修订人    描述
  */
 public class TimeServerHandler extends ChannelHandlerAdapter {
-
+	
+	private int count;
+	
+	/**
+	 * 当客户端主动链接服务端的链接后，这个通道就是活跃的了。
+	 * 也就是客户端与服务端建立了通信通道并且可以传输数据
+	 */
 	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf buf = (ByteBuf) msg;
-		byte[] req = new byte[buf.readableBytes()];
-		buf.readBytes(req);
-		String body = new String(req, "utf-8");
-		System.out.println("server receive : " + body);
-		
-		ByteBuf resp = Unpooled.copiedBuffer(new Date().toString().getBytes());
-		ctx.write(resp);
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println(ctx.channel().localAddress().toString() + " channelActive");
 	}
 	
+	/**
+	 * 当客户端主动断开服务端的链接后，这个通道就是不活跃的。
+	 * 也就是说客户端与服务端的关闭了通信通道并且不可以传输数据
+	 */
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println(ctx.channel().localAddress().toString() + " channelInactive");
+	}
+
+	/**
+	 * 简而言之就是从通道中读取数据，也就是服务端接收客户端发来的数据
+	 * 但是这个数据在不进行解码时它是ByteBuf类型
+	 */
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		String body = (String) msg;
+		System.out.println("server receive : " + body + ++count);
+		//响应数据加上换行
+		String message = new Date().toString() + System.getProperty("line.separator");
+		ByteBuf resp = Unpooled.copiedBuffer(message.getBytes());
+		ctx.writeAndFlush(resp);
+	}
+	
+	/**
+	 * 在通道读取完成后会在这个方法里通知，对应可以做刷新操作
+	 */
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		ctx.flush();
 	}
 	
+	/**
+	 * 抓住异常，当发生异常的时候，可以做一些相应的处理，比如打印日志、关闭链接
+	 */
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		ctx.close();
+		System.out.println("异常信息：\r\n"+cause.getMessage());
 	}
 	
 }
